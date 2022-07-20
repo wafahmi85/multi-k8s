adf4700cca5757c39334abc988aec0790e70c0ea
Fahmi Rasdi <fahmi@Fahmis-MacBook-Pro.local>
2022-07-21 00:42:48 +0800

    add deploy script 5

--- deploy.sh
+++ deploy.sh
@@ -1,17 +1,17 @@
-docker build -t wafahmi/multi-client:latest -t wafahmi/multi-client:$SHA -f ./client/Dockerfile ./client
-docker build -t wafahmi/multi-server:latest -t wafahmi/multi-api:$SHA -f ./server/Dockerfile ./server
-docker build -t wafahmi/multi-worker -t wafahmi/multi-worker:$SHA -f ./worker/Dockerfile ./worker
+# docker build -t wafahmi/multi-client:latest -t wafahmi/multi-client:$SHA -f ./client/Dockerfile ./client
+# docker build -t wafahmi/multi-api:latest -t wafahmi/multi-api:$SHA -f ./server/Dockerfile ./server
+# docker build -t wafahmi/multi-worker -t wafahmi/multi-worker:$SHA -f ./worker/Dockerfile ./worker
 
-docker push wafahmi/multi-client:latest
-docker push wafahmi/multi-api:latest
-docker push  wafahmi/multi-worker:latest
+# docker push wafahmi/multi-client:latest
+# docker push wafahmi/multi-api:latest
+# docker push  wafahmi/multi-worker:latest
 
-docker push wafahmi/multi-client:$SHA
-docker push wafahmi/multi-api:$SHA
-docker push  wafahmi/multi-worker:$SHA 
+# docker push wafahmi/multi-client:$SHA
+# docker push wafahmi/multi-api:$SHA
+# docker push  wafahmi/multi-worker:$SHA 
 
-kubectl apply -f k8s
+kubectl delete -f k8s
 
-kubectl set image deployments/client-deployment server=wafahmi/multi-client:$SHA
-kubectl set image deployments/server-deployment server=wafahmi/multi-api:$SHA
-kubectl set image deployments/worker-deployment server=wafahmi/multi-worker :$SHA
+# kubectl set image deployments/client-deployment server=wafahmi/multi-client:$SHA
+# kubectl set image deployments/server-deployment server=wafahmi/multi-api:$SHA
+# kubectl set image deployments/worker-deployment server=wafahmi/multi-worker :$SHA
